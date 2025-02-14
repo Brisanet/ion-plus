f07f031f3d1fe998fecb2aded7da8a548410e886
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2025-02-14 09:15:10 -0300

    feat: structure

---
+++ projects/ion/src/lib/select/select.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ion-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.scss'],
+})
+export class IonSelectComponent {}
