1e3d03e50e6eaa6785273bdd512400aa94ced747
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2025-02-14 09:30:06 -0300

    feat: add toggle, options and placeholder

--- projects/ion/src/lib/select/select.component.ts
+++ projects/ion/src/lib/select/select.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 
 @Component({
   selector: 'ion-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
 })
-export class IonSelectComponent {}
+export class IonSelectComponent {
+  isDropdownOpen = signal(false);
+  options = input<{ label: string; value: string }[]>();
+  placeholder = input('');
+
+  toggleDropdown() {
+    this.isDropdownOpen.set(!this.isDropdownOpen());
+  }
+}
