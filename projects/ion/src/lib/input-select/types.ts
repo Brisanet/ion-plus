995de1648b732c82d9b9ef9497d4ffc13e6ac9f8
larissa-kamily-brisa <larissa.kamily@grupobrisanet.com.br>
2025-03-28 12:54:44 -0300

    feat: adding input select types

---
+++ projects/ion/src/lib/input-select/types.ts
@@ -0,0 +1,47 @@
+import { IonDropdownOption } from '../directives/dropdown';
+
+export interface SelectOption extends IonDropdownOption {
+  multiple?: boolean;
+  firstPlaceholder?: string;
+  secondPlaceholder?: string;
+}
+
+export type ValueToEmmit = {
+  optionSelected: SelectOption;
+  firstValue: string;
+  secondValue?: string;
+};
+
+export interface IonInputSelectProps {
+  /**
+   * @param name - Defines de id of the input.
+   * @type {string}
+   * @example
+   * <ion-input-select [name]="test"></ion-input-select>
+   */
+  name: string;
+  /**
+   * @param disabled - Defines the state disabled of the input.
+   * @type {boolean}
+   * @default `false``
+   */
+  disabled: boolean;
+  /**
+   * @param selecteOptions - Defines the options of the select dropdown.
+   * @type {SelectOption}
+   * @default `defaultSelectOptions`
+   */
+  selectOptions: SelectOption;
+  /**
+   * @param valid - Defines the state of valid or invalid.
+   * @type {boolean}
+   * @default `true`
+   */
+  valid: boolean;
+  /**
+   * @event valueChange - Event triggered when a change occurs in the input select.
+   * @type {ValueToEmmit}
+   * @description - This event is trigerred when the input receives a value in any field and/or a option is selected in the dropdown.
+   */
+  valueChange: ValueToEmmit;
+}
