63f0708e5bea870871ca21e0839c3d2689fd976a
Breno-silva-brisa <breno.silva@timebrisa.com.br>
2025-08-21 09:48:59 -0300

    feat(button): add options and dropdownConfig to the button component

--- projects/ion/src/lib/directives/dropdown/mocks/open-dropdown.component.ts
+++ projects/ion/src/lib/directives/dropdown/mocks/open-dropdown.component.ts
@@ -2,6 +2,7 @@ import { Component, input } from '@angular/core';
 import { IonDropdownConfig, IonDropdownOption } from '../types';
 import { IonButtonComponent } from '../../../button';
 import { IonDropdownDirective } from '../dropdown.directive';
+import { SafeAny } from '../../../utils/safe-any';
 
 export interface Character extends IonDropdownOption {
   name: string;
@@ -22,4 +23,6 @@ export class OpenDropdownComponent {
   dropdownConfig = input<IonDropdownConfig<Character>>({});
   dropdownLoading = false;
   dropdownOptions: Character[] = [];
+  dropdownEvent: SafeAny;
+  dropdownRef: SafeAny;
 }
